217b7d142bb908f9770a1305df5c62dfee31530e
Je Xia <i@jex.me>
2025-09-14 10:10:31 +0800

    Typo

--- src/main.rs
+++ src/main.rs
@@ -259,7 +259,7 @@ fn print_help_and_exit() {
 Options:
   -w, --width <width>     Set the width of the output image
   -h, --height <height>   Set the height of the output image
-  -fit <fit>              Set the fit mode for the resize operation [possible values: cover, contain, scale-down]
+  --fit <fit>              Set the fit mode for the resize operation [possible values: cover, contain, scale-down]
     --cover               Resize the image to fill the given dimensions, cropping if necessary
     --contain             Resize the image to fit the given dimensions
     --scale-down          Resize the image to fit the given dimensions, but not larger than the original
